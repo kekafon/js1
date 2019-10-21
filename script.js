bee3a7f90288862af01cbb0f7ba5b03320d11b7c
mercool <mistik4700@gmail.com>
2019-10-21 09:38:37 +0300

    Добавлены циклы в приложение

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-'use strict';
-
 var money = +prompt('Ваш бюджет на месяц?', '');
 var time = prompt('Введите дату в формате YYYY-MM--DD', '');
-
-var stat1 = prompt('Введите обязательную статью расходов?', '');
-var moneyMax1 = +prompt('Во сколько обойдётесь?', '');
-var stat2 = prompt('Введите обязательную статью расходов?', '');
-var moneyMax2 = +prompt('Во сколько обойдётесь?', '');
 
 var appData = {
     budget: money,
     timeData: time,
-    expenses: {stat1 : moneyMax1, stat2: moneyMax2},
+    expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: false,
 };
 
-alert('Расход в день ' + (appData.budget  / 30));
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Введите обязательную статью расходов?', '');
+    let b = prompt('Во сколько обойдётесь?', '');
+
+    if (typeof(a) === 'string' && typeof(a) != 'null' && typeof(b) === 'string' && typeof(b) != 'null' && a != '' && b != '' && a.length < 50) {
+        console.log('done');
+        appData.expenses[a] = b;    
+    } else {
+        console.log('Bad result');
+        i--;
+    } 
+};
+
+appData.moneyPerDay = (appData.budget / 30);
+
+alert('Расход в день ' + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log('Минимальный уровень достатка');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+    console.log('Средний уровень достатка');
+} else if (appData.moneyPerDay > 2000) {
+    console.log('Высокий уровень достатка');
+} else {
+    console.log('Error');
+}
